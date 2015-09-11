39b5c7e0a735007c872c05601bb2511d5ca5a5f7
Philip Dexter <philip.dexter@gmail.com>
2015-09-11 10:06:52 -0400

    Update manage.hs

--- manage.hs
+++ manage.hs
@@ -1,9 +1,11 @@
 #!/usr/bin/env runhaskell
 
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 import Turtle
 import Control.Monad (forM_)
+import Data.Text (strip)
 
 main = do
   dir <- pwd
@@ -11,15 +13,18 @@ main = do
   forM_ files $ \(f:fullpath) -> do
          let full = dir </> (fromString f)
          copyTo <- case fullpath of
-                     []       -> fmap (<>fromString("."<>f)) home
-                     (path:_) -> fmap (<>fromString path) home
-         exists <- testfile copyTo
-         let flag = if exists then "." else "*"
-         isLink <- if not exists
-                   then return ExitSuccess
-                   else shell (format ("[ -L "%fp%" ]") copyTo) empty
-         case isLink of
-           ExitSuccess -> do
-                            echo (format (s%" "%fp%" --> "%fp) flag full copyTo)
-                            shell (format ("ln -sf "%fp%" "%fp) full copyTo) empty
-           _           -> echo (format ("WARNING: "%fp%" is not a symlink , NOT LINKING") copyTo) >> return ExitSuccess
+           []       -> fmap (<>fromString("."<>f)) home
+           (path:_) -> fmap (<>fromString path) home
+         testfile copyTo >>= \case
+           True -> do
+             shell (format ("[ -L "%fp%" ]") copyTo) empty >>= \case
+               ExitSuccess   -> do
+                 (_, linksTo) <- (fmap . fmap) (fromText . strip) $ shellStrict (format ("readlink "%fp) copyTo) empty
+                 echo (if linksTo == full
+                       then format ("nothing to do: "%fp%" already linked to "%fp) copyTo full
+                       else format ("WARNING: "%fp%" linked to another file: "%fp) copyTo linksTo)
+               ExitFailure _ -> do
+                  echo (format ("WARNING: "%fp%" is not a symlink , NOT LINKING") copyTo)
+           False -> void $ do
+             echo (format ("linking: "%fp%" --> "%fp) full copyTo)
+             shell (format ("ln -sf "%fp%" "%fp) full copyTo) empty
