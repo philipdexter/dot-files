e1fc87876550fd5d8d55e1a1c6b3375599391b2d
Philip Dexter <philip.dexter@wolt.com>
2022-11-22 11:15:57 +0200

    Hammerspoon

--- init.lua
+++ init.lua
@@ -1,32 +1,90 @@
+-- hs.hotkey.bind({"alt", "shift"}, "r", function()
+--   hs.application.launchOrFocus("Slack")
+-- end)
+
+-- hs.hotkey.bind({"alt", "shift"}, "f", function()
+--   hs.application.launchOrFocus("iTerm")
+-- end)
+
+-- hs.hotkey.bind({"alt", "shift"}, "d", function()
+--   hs.application.launchOrFocus("Google Chrome")
+-- end)
+--
+
+-- hs.hotkey.bind({"cmd", "shift"}, "8", function()
+--   hs.execute("~/switch_external.sh")
+-- end)
+
+-- hs.hotkey.bind({"cmd", "shift"}, "7", function()
+--   hs.execute("~/switch_internal.sh")
+-- end)
+
+hs.midi.deviceCallback(function(devices, virtualDevices)
+     print(hs.inspect(devices))
+     print(hs.inspect(virtualDevices))
+     open = false
+     if next(virtualDevices) ~= nil then
+         return
+     end
+     for _, l in ipairs(devices) do
+         if l == "SL STUDIO" then
+             open = true
+         end
+     end
+     if open then
+         hs.execute("open ~/Music/Logic/instruments.logicx", true)
+     else
+         app = hs.application.get("Logic Pro")
+         if app ~= nil then
+             app:kill()
+         end
+     end
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "c", function()
+    local device = hs.audiodevice.findOutputByName("External Headphones")
+    if device then
+        device:setDefaultOutputDevice()
+    end
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "x", function()
+    local device = hs.audiodevice.findOutputByName("Scarlett Solo USB")
+    if device then
+        device:setDefaultOutputDevice()
+    end
+end)
+
+
 hs.hotkey.bind({"cmd", "shift"}, "v", function()
   hs.application.launchOrFocus("MuseScore 3")
 end)
 
-hs.hotkey.bind({"cmd", "shift"}, "r", function()
-  hs.application.launchOrFocus("iTerm")
-end)
-
 hs.hotkey.bind({"cmd", "shift"}, "i", function()
-  hs.execute("cmus-remote -u", true)
+  hs.spotify.playpause()
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "u", function()
-  hs.execute("cmus-remote --prev", true)
+  hs.spotify.previous()
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "o", function()
-  hs.execute("cmus-remote --next", true)
+  hs.spotify.next()
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "p", function()
-  hs.osascript.applescript('tell application "iTerm2" \n set newiterm to (create window with default profile) \n activate \n tell current session of newiterm \n write text "tma music" \n  end tell \n end tell')
+  hs.application.launchOrFocus("Spotify")
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "f", function()
-  hs.application.launchOrFocus("Google Chrome")
+  hs.application.launchOrFocus("iTerm")
 end)
 
 hs.hotkey.bind({"cmd", "shift"}, "d", function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "r", function()
   hs.application.launchOrFocus("Slack")
 end)
 
@@ -67,7 +125,7 @@ hs.hotkey.bind({"cmd", "shift"}, "n", function()
 end)
 
 hs.hotkey.bind({"cmd", "ctrl"}, "l", function()
-  hs.caffeinate.systemSleep()
+  hs.spotify.playpause()
 end)
 
 hs.hotkey.bind({"cmd"}, "\\", function()
