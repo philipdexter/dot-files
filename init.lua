749f98bac1edaa75727b24cc409e0a44d5dde06c
Philip Dexter <philip.dexter@wolt.com>
2024-07-11 16:44:59 +0300

    Update hammerspoon

--- init.lua
+++ init.lua
@@ -1,3 +1,46 @@
+-- tiling
+
+hs.grid.setGrid(hs.geometry.size(2,1))
+
+hs.hotkey.bind({"alt", "ctrl"}, "f", function()
+    window = hs.window.focusedWindow()
+    hs.grid.maximizeWindow(window)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "s", function()
+    window = hs.window.focusedWindow()
+    hs.grid.snap(window)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "h", function()
+    window = hs.window.focusedWindow()
+    if window:size().w > 1000 then
+        hs.grid.resizeWindowThinner(window)
+    end
+    hs.grid.pushWindowLeft(window)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "l", function()
+    window = hs.window.focusedWindow()
+    if window:size().w > 1000 then
+        hs.grid.resizeWindowThinner(window)
+    end
+    hs.grid.pushWindowRight(window)
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "h", function()
+    window = hs.window.focusedWindow()
+    window:focusWindowWest()
+end)
+
+
+hs.hotkey.bind({"cmd", "ctrl"}, "l", function()
+    window = hs.window.focusedWindow()
+    window:focusWindowEast()
+end)
+
+-- app switching
+
 hs.hotkey.bind({"cmd", "shift"}, "v", function()
   hs.application.launchOrFocus("Things3")
 end)
@@ -18,3 +61,61 @@ hs.hotkey.bind({"cmd", "shift"}, "c", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
+
+hs.hotkey.bind({"cmd", "shift"}, "i", function()
+  _, folder = hs.dialog.textPrompt("Note opener 9000", "Who?")
+  hs.applescript.applescript([[
+    tell application "Notes"
+      tell account "iCloud"
+          tell folder "Wolt"
+            tell folder "1:1"
+              show folder "]] .. folder .. [["
+            end tell
+          end tell
+      end tell
+    end tell
+  ]])
+  hs.application.launchOrFocus("Notes")
+  hs.applescript.applescript([[
+    tell application "System Events" to tell process "Notes"
+      click menu item "Open Note in New Window" of menu 1 of menu bar item "Window" of menu bar 1
+    end tell
+  ]])
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "o", function()
+  _, folder = hs.dialog.textPrompt("Note opener 9000", "Who?")
+  hs.applescript.applescript([[
+    tell application "Notes"
+      tell account "iCloud"
+          tell folder "Wolt"
+            tell folder "1:1"
+              show folder "]] .. folder .. [["
+              open note 1
+            end tell
+          end tell
+      end tell
+    end tell
+  ]])
+  hs.application.launchOrFocus("Notes")
+end)
+
+
+hs.hotkey.bind({"cmd", "shift"}, "return", function()
+  if hs.application.find("iTerm") then
+    hs.applescript.applescript([[
+      tell application "iTerm"
+        create window with default profile
+      end tell
+    ]])
+    hs.application.launchOrFocus("iTerm")
+  else
+    hs.application.open("iTerm")
+  end
+end)
+
+hs.hotkey.bind({"cmd"}, "\\", function()
+  date, _, _ = hs.execute("date +'%Y-%m-%d'")
+  hs.eventtap.keyStroke({"cmd", "shift"}, "h")
+  hs.eventtap.keyStrokes(date)
+end)
